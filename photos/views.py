0e3f5d0c05aeeffda246b08206001788f43117fb
Alberto Casero <alberto@kasfactory.net>
2016-09-08 22:03:05 +0200

    Implementamos el detalle de foto

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseNotFound, HttpResponse
 from django.shortcuts import render
 from photos.models import Photo, VISIBILITY_PUBLIC
 
@@ -12,3 +13,21 @@ def home(request):
     photos = Photo.objects.filter(visibility=VISIBILITY_PUBLIC).order_by('-created_at')
     context = {'photos_list': photos[:4]}
     return render(request, 'photos/home.html', context)
+
+
+def photo_detail(request, pk):
+    """
+    Renderiza el detalle de una imagen
+    :param request: objeto HttpRequest con los datos de la petición
+    :param pk: clave primaria de la foto a recuperar
+    :return: objeto HttpResponse con los datos de la respuesta
+    """
+    possible_photos = Photo.objects.filter(pk=pk)
+    if len(possible_photos) == 0:
+        return HttpResponseNotFound("La imagen que buscas no existe")
+    elif len(possible_photos) > 1:
+        return HttpResponse("Múltiples opciones", status=300)
+
+    photo = possible_photos[0]
+    context = {'photo': photo}
+    return render(request, 'photos/photo_detail.html', context)
