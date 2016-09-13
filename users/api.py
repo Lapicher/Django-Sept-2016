b479c8c01cc891cf0c6b367bf87c1e19f5da3c22
Alberto Casero <alberto@kasfactory.net>
2016-09-14 00:06:16 +0200

    Usamos APIView y Response de REST Framework

--- users/api.py
+++ users/api.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.views import View
-from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from users.serializers import UserSerializer
 
 __author__ = 'kas'
 
 
-class UserListAPI(View):
+class UserListAPI(APIView):
     """
     Endpoint de listado de usuarios
     """
     def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
-        renderer = JSONRenderer()
-        data = renderer.render(serializer.data)
-        return HttpResponse(data)
+        return Response(serializer.data)
