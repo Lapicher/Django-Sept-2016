58429c5653bc4a30200ff9b815cb174fbb19f477
Alberto Casero <alberto@kasfactory.net>
2016-09-16 00:02:16 +0200

    Filtramos contenidos din´ámicamente en nuestro API de Photos

--- photos/api.py
+++ photos/api.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from photos.models import Photo
 from photos.serializers import PhotoSerializer, PhotoListSerializer
+from photos.views import PhotoQueryset
 
 __author__ = 'kas'
 
@@ -12,8 +13,10 @@ class PhotoListAPI(ListCreateAPIView):
     """
     Endpoint de listado y creación de fotos
     """
-    queryset = Photo.objects.all()
     permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        return PhotoQueryset.get_photos_by_user(self.request.user)
 
     def get_serializer_class(self):
         return PhotoSerializer if self.request.method == 'POST' else PhotoListSerializer
@@ -23,6 +26,8 @@ class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
     """
     Endpoint de detalle, actualización y borrado de fotos
     """
-    queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        return PhotoQueryset.get_photos_by_user(self.request.user)
