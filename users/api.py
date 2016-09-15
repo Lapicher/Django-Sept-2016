e8cd3899c715692a5a996112b198e2d24ca6214b
Alberto Casero <alberto@kasfactory.net>
2016-09-15 21:21:49 +0200

    Creamos endpoint de actualizacion de usuario

--- users/api.py
+++ users/api.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_202_ACCEPTED
 
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, UserListSerializer
 
 __author__ = 'kas'
 
@@ -15,7 +16,7 @@ class UserListAPI(APIView):
     """
     def get(self, request):
         users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
+        serializer = UserListSerializer(users, many=True)
         return Response(serializer.data)
 
     def post(self, request):
@@ -25,3 +26,24 @@ class UserListAPI(APIView):
             return Response(serializer.data, status=HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+class UserDetailAPI(APIView):
+    """
+    Endpoint de detalle de un usuario
+    """
+    def get(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
